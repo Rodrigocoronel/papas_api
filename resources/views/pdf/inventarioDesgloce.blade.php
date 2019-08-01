4ec99db83b7e439e142f12caf5c1051b6fee40df
Rodrigocoronel <rodrigo.coronel.14@gmail.com>
2019-07-31 19:52:04 -0700

    pdfs

---
+++ resources/views/pdf/inventarioDesgloce.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>reporte</title>
+	<style type="text/css">
+		
+		.utilidad tr:nth-child(even) {background-color: #f2f2f2;}
+
+		table.utilidad{
+			width: 100%;
+			border-collapse: collapse;
+		}
+
+		body{
+			width: 100%;
+		}
+		html{
+			width: 100%;
+		}
+
+	</style>
+	
+</head>
+<body>
+	<h4>Inventario almacen {{$almacen}}</h4>
+	<table class="utilidad" border="1px solid">
+		<thead>
+			<tr>
+				<th width="15%">Folio</th>
+				<th width="15%">Insumo</th>
+				<th width="15%">Desc.</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($data as $todo => $dataValues)
+				<tr>
+					<td align="center">{{$dataValues['folio']}}</td>
+					<td align="center">{{$dataValues['insumo']}}</td>
+					<td align="center">{{$dataValues['desc']}}</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+</body>
+</html>
